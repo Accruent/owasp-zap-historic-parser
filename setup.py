ffffe4517ba2a716a388e893c5f573297b0785bd
neil.howell <neil.howell@accruent.com>
2020-06-09 08:46:34 -0500

    QE-1939 setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""This module is the setup for OWASP ZAP Historic Parser"""
+from setuptools import find_packages, setup
+
+setup(
+    name='owasp-zap-historic-parser',
+    version="0.1.0",
+    description='Parser to push OWASP ZAP report data to MySQL',
+    classifiers=[
+        'Framework :: OWASP ZAP',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ],
+    keywords='owasp zap historical report parser',
+    author='Neil Howell',
+    author_email='neiljhowell@gmail.com',
+    url='https://github.com/Accruent/owasp-zap-historic-parser',
+    license='GPL-3.0',
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+        'mysql-connector',
+    ],
+    entry_points={
+        'console_scripts': [
+            'owaspzaphistoricparser=owasp_zap_historic_parser.runner:main',
+        ]
+    },
+)
