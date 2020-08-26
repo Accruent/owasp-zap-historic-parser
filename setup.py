7465db5b5910dd6791449c1bdb44bdfaae1ab5d2
neil.howell <neil.howell@accruent.com>
2020-08-26 15:24:46 -0500

    QE-1998 fix time to match UTC to CDT conversion that happens in OZH application

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 """This module is the setup for OWASP ZAP Historic Parser"""
 from setuptools import find_packages, setup
-
+import version
 
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     print(LONG_DESCRIPTION)
 
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.read().splitlines()
+
 
 setup(
     name='owasp-zap-historic-parser',
-    version="0.1.5",
+    version=version.VERSION,
     description='Parser to push OWASP ZAP report data to MySQL and generate delta report',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
@@ -27,9 +30,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
 
-    install_requires=[
-        'mysql-connector',
-    ],
+    install_requires=REQUIREMENTS,
     entry_points={
         'console_scripts': [
             'owaspzaphistoricparser=owasp_zap_historic_parser.runner:main',
