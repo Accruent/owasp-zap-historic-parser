3dc952b40ea52455a2aca6d686f26edac953a60c
neil.howell <neil.howell@accruent.com>
2020-09-02 11:55:22 -0500

    QE-2074 add unit tests for process zap file

--- test/db_test.py
+++ test/db_test.py
@@ -3,8 +3,8 @@ import sys
 import unittest
 from unittest.mock import patch, MagicMock, call
 
-from owasp_zap_historic_parser.owasp_zap_historical import connect_to_mysql_db
-from owasp_zap_historic_parser.runner import main
+from owasp_zap_historic_parser.owasp_zap_historical import connect_to_mysql_db, process_zap_file
+from owasp_zap_historic_parser.runner import main, parse_options
 
 
 class TestDBFunctions(unittest.TestCase):
@@ -12,11 +12,11 @@ class TestDBFunctions(unittest.TestCase):
 
     @patch('owasp_zap_historic_parser.runner.process_zap_file')
     # pylint: disable=R0201
-    def test_main(self, process_zap_file):
+    def test_main(self, pzf_mock):
         """Tests main function"""
         sys.argv[1:] = ['-f', 'test_files/testReport.html']
         main()
-        process_zap_file.assert_called()
+        pzf_mock.assert_called()
 
     @patch('mysql.connector.connect')
     def test_connect_to_mysql_db(self, connect_mock):
@@ -28,3 +28,17 @@ class TestDBFunctions(unittest.TestCase):
         self.assertEqual(connect_mock.call_args_list[0], call(host=args[0], port=args[1],
                                                               user=args[2], passwd=args[3],
                                                               database=args[4]))
+
+    @patch('owasp_zap_historic_parser.owasp_zap_historical.html_parser')
+    @patch('mysql.connector.connect')
+    @patch('owasp_zap_historic_parser.owasp_zap_historical.process_zap_results',
+           return_value='some html code')
+    def test_process_zap_file(self, mock_hp, mock_connect, mock_pzr):
+        """Tests the process zap file function"""
+        sys.argv[1:] = ['-f', 'test_files/testReport.html']
+        options = parse_options()
+        result = process_zap_file(options)
+        self.assertEqual(1, mock_hp.call_count)
+        self.assertEqual(2, mock_connect.call_count)
+        self.assertEqual(1, mock_pzr.call_count)
+        self.assertEqual(result, 'some html code')
