64df96bd5036189703d0684138e2601da74312ae
neil.howell <neil.howell@accruent.com>
2020-09-02 10:47:27 -0500

    QE-2074 add unit tests for connect_to_mysql_db

--- test/db_test.py
+++ test/db_test.py
@@ -1,8 +1,9 @@
 """Unit tests for functions using db in OWASP ZAP Historic Parser"""
 import sys
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock, call
 
+from owasp_zap_historic_parser.owasp_zap_historical import connect_to_mysql_db
 from owasp_zap_historic_parser.runner import main
 
 
@@ -16,3 +17,14 @@ class TestDBFunctions(unittest.TestCase):
         sys.argv[1:] = ['-f', 'test_files/testReport.html']
         main()
         process_zap_file.assert_called()
+
+    @patch('mysql.connector.connect')
+    def test_connect_to_mysql_db(self, connect_mock):
+        """Tests the mysql connection function"""
+        connect_mock.return_value = MagicMock(name='connection_return')
+        args = (1, 2, 3, 4, 5)
+        connect_to_mysql_db(*args)
+        self.assertEqual(1, connect_mock.call_count)
+        self.assertEqual(connect_mock.call_args_list[0], call(host=args[0], port=args[1],
+                                                              user=args[2], passwd=args[3],
+                                                              database=args[4]))
